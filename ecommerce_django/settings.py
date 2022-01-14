c08cfb3a9ffa1d1ac72d6b3eef74baae19c0ef42
Ana Navarro <ana.elisacn.98@gmail.com>
2022-01-14 17:27:35 -0300

    End of First Video

--- ecommerce_django/settings.py
+++ ecommerce_django/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'ecommerce_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.views.categories'
             ],
         },
     },
